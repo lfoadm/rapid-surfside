8c7d090ced2967632423a63973a1c5ecaa35fe3f
Leandro <lfoadm@icloud.com>
2024-09-03 22:44:44 -0300

    fiters price, brands and categories

--- app/Http/Controllers/Site/ShopController.php
+++ app/Http/Controllers/Site/ShopController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Brand;
+use App\Models\Admin\Category;
 use App\Models\Admin\Product;
 use Illuminate\Http\Request;
 
@@ -14,6 +16,11 @@ class ShopController extends Controller
         $o_column = "";
         $o_order = "";
         $order = $request->query('order') ? $request->query('order') : -1;
+        $f_brands = $request->query('brands');
+        $f_categories = $request->query('categories');
+        $min_price = $request->query('min') ? $request->query('min') : 1;
+        $max_price = $request->query('max') ? $request->query('max') : 50000;
+
         switch($order)
         {
             case 1:
@@ -36,8 +43,22 @@ class ShopController extends Controller
                 $o_column='id';
                 $o_order='DESC';
         }
-        $products = Product::orderBy($o_column, $o_order)->paginate($size);
-        return view('site.shop', compact('products', 'size', 'order'));
+        
+        $brands = Brand::orderBy('name', 'ASC')->get();
+        $categories = Category::orderBy('name', 'ASC')->get();
+
+        $products = Product::where(function($query) use($f_brands){
+            $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'".$f_brands."'=''");
+        })
+        ->where(function($query) use($f_categories){
+            $query->whereIn('category_id', explode(',', $f_categories))->orWhereRaw("'".$f_categories."'=''");
+        })
+        ->where(function($query) use($min_price, $max_price){
+            $query->whereBetween('regular_price', [$min_price, $max_price])->orWhereBetween('sale_price', [$min_price, $max_price]);
+        })
+        ->orderBy($o_column, $o_order)->paginate($size);
+
+        return view('site.shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'min_price', 'max_price'));
     }
 
     public function product_show($product_slug)
